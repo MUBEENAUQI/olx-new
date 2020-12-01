cd56f9334f284c9b2a4c616585a0c263879189d6
srujana207 <41240100+srujana207@users.noreply.github.com>
2020-12-01 15:58:06 +0530

    Update sell.dart

--- lib/sell.dart
+++ lib/sell.dart
@@ -25,7 +25,6 @@ class SellScreenState extends State<Sell> {
           Widget>[
         Container(
           width: MediaQuery.of(context).size.width,
-         height: MediaQuery.of(context).size.height,
           child: Padding(
             padding: const EdgeInsets.fromLTRB(0.0, 5.0, 8.0, 0.0),
             child: Container(
@@ -85,7 +84,7 @@ class SellScreenState extends State<Sell> {
         GridView.count(
             shrinkWrap: true,
             crossAxisCount: 2,
-            childAspectRatio: MediaQuery.of(context).size.height / 250,
+            childAspectRatio: MediaQuery.of(context).size.height / 400,
             padding: const EdgeInsets.all(15),
             children: <Widget>[
               GestureDetector(
